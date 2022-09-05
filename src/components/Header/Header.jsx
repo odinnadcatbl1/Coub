34f37aaf5c28aa483241d6d34c31488b81fbaa86
Salavat F <salmrproperfahr@mail.ru>
2022-09-05 12:41:06 +0300

    done header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,53 @@
 import logo from "../../assets/logo.svg";
+import introImg from "../../assets/intro.png";
 
 const Header = () => {
     return (
         <header className="header">
             <div className="container">
                 <div className="header__inner">
-                    <div className="header__logo">
-                        <div className="logo__container">
-                            <img src={logo} alt="logo" className="logo__img" />
+                    <div className="header__left">
+                        <div className="header__logo">
+                            <div className="logo__container">
+                                <img
+                                    src={logo}
+                                    alt="logo"
+                                    className="logo__img"
+                                />
+                            </div>
+
+                            <div className="logo-name"> logo</div>
                         </div>
 
-                        <div className="logo-name"> logo</div>
+                        <div className="header__intro">
+                            <h1 className="page__title">text_text_text</h1>
+                            <div className="intro__text">
+                                text_text_text_text
+                            </div>
+
+                            <button className="intro__button">
+                                text_text_text
+                            </button>
+                        </div>
                     </div>
 
-                    <nav className="header__nav">
-                        <ul className="nav__list">
-                            <li className="nav__item">text</li>
-                            <li className="nav__item">text_text</li>
-                            <li className="nav__item">text_text</li>
-                        </ul>
-                    </nav>
+                    <div className="header__right">
+                        <nav className="header__nav">
+                            <ul className="nav__list">
+                                <li className="nav__item">text</li>
+                                <li className="nav__item">text_text</li>
+                                <li className="nav__item">text_text</li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <div className="intro__img-container">
+                        <img
+                            src={introImg}
+                            alt="intro"
+                            className="intro__img"
+                        />
+                    </div>
                 </div>
             </div>
         </header>
