1c1b57162b6d1db136141ce2f317edc7c6b822cd
Salavat F <salmrproperfahr@mail.ru>
2022-09-05 10:46:50 +0300

    create structure of project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <h1>hello</h1>
+        <div className="container">
+            <h1>hello</h1>
+        </div>
     </React.StrictMode>
 );
